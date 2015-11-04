f0c7739dc1ea94d568aac63f00ac0e3fd16fdd73
xuld <xuld@vip.qq.com>
2015-11-04 16:49:18 +0800

    优化

--- bin/tpack-cli.js
+++ bin/tpack-cli.js
@@ -1,5 +1,7 @@
 ﻿#!/usr/bin/env node
 
+const defaultTpackConfig = "tpack.config.js";
+
 var FS = require("fs");
 var Path = require('path');
 var tpack = module.exports = require('../lib/index.js');
@@ -10,59 +12,91 @@ function main(argv) {
     
     // 解析命令行参数。
     var options = parseArgv(argv);
-    var cmdName = argv[0] || 'default';
     
-    // 预设命令参数。
-    tpack.srcPath = options.src || options['in'] || options.i || tpack.srcPath;
-    tpack.destPath = options.dest || options.out || options.o || tpack.destPath;
-    tpack.logLevel = isNaN(+options.level) ? options.all ? 6 : tpack.logLevel : +options.level;
-    tpack.verbose = options.verbose || options.debug || options.d || tpack.verbose;
-    
-    // 处理全局指令。
-    if (options.v || options.version) {
-        tpack.log("CLI Version: " + require('../package.json').version);
+    // -v, -version, --version
+    if (options.version = options.v || options.version || options["-version"]) {
+        console.log(require('../package.json').version);
         return;
     }
-    
-    if (options.h || options.help || options["?"]) {
-        options.help = cmdName;
-        cmdName = 'help';
+
+    // -cwd, --cwd
+    if (options.cwd = options.cwd || options["-cwd"]) {
+        process.cwd(options.cwd);
     }
     
+    // -h, -help, -?, --help
+    if (options.help = options.h || options.help || options["?"]) {
+        options.help = Array.prototype.slice.call(options);
+        options[0] = 'help';
+        options.length = 1;
+    }
+    
+    // -src, -in, -i
+    if (options.src = options.src || options['in'] || options.i) {
+        tpack.srcPath = options.src;
+    }
+
+    // -dest, -out, -o
+    if (options.dest = options.dest || options.out || options.o) {
+        tpack.destPath = options.dest;
+    }
+    
+    // -verbose, -debug, -d
+    if (options.verbose = options.verbose || options.debug || options.d) {
+        tpack.verbose = true;
+    }
+    
+    // -error, -e, -info, -silient, -s, -log
+    tpack.logLevel = options.error || options.e ? tpack.LOG_LEVEL.error :
+        options.log ? tpack.LOG_LEVEL.log :
+        options.info ? tpack.LOG_LEVEL.info :
+        options.silient || options.s ? tpack.LOG_LEVEL.none :
+        tpack.logLevel;
+    
     // 支持载入全局模块。
-    require('require-global')([Path.resolve(__dirname, "../../"), Path.resolve(__dirname, "../node_modules/")]);
-        
+    try {
+        require('require-global')([Path.resolve(__dirname, "../../"), Path.resolve(__dirname, "../node_modules/")]);
+    } catch (e) {
+    }
+
+    // -config, --config, 
+    options.config = Path.resolve(options.config || options['-config'] || defaultTpackConfig);
+    
     // 执行 tpackCfg.js
-    var tpackCfg = process.cwd() + '/tpackCfg.js';
-    if (FS.existsSync(tpackCfg)) {
-        require(tpackCfg);
+    if (FS.existsSync(options.config)) {
+        require(options.config);
     }
     
     // 驱动主任务。 
-    tpack.task(cmdName, options);
+    for (var i = 0; i < options.length; i++) {
+        tpack.task(options[i], options);
+    }
 
 }
 
 /**
  * 解析命令提示符参数。
+ * @param {Array} 原始参数数组。
+ * @returns {Object} 返回键值对象。
+ * @example 
+ * parseArgv(["a", "-c1", "-c2", "v", "b"]) // {0: "a", c1: true, c2: "v", 1: "b"}
  */
 function parseArgv(argv) {
-    var obj = {};
+    var result = {length: 0};
     for (var i = 0; i < argv.length; i++) {
         var arg = argv[i];
-        if (/^(\-|\/)/.test(arg)) {
+        if (/^[\-\/]/.test(arg)) {
             var value = argv[i + 1];
-            var nextIsCommand = !value || /^(\-|\/)/.test(value);
+            var nextIsCommand = !value || /^[\-\/]/.test(value);
             if (nextIsCommand) {
                 value = true;
-                argv.splice(i, 1);
-                i--;
             } else {
-                argv.splice(i, 2);
-                i -= 2;
+                i++;
             }
-            obj[arg.slice(1)] = value;
+            result[arg.substr(1)] = value;
+        } else {
+            result[result.length++] = arg;
         }
     }
-    return obj;
+    return result;
 }
