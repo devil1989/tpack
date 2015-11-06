d8344a47f399127c6eb9c1dbfc0cf74297a6dc48
xuld <xuld@vip.qq.com>
2015-11-06 20:21:02 +0800

    模块文件

--- README.md
+++ README.md
@@ -1,125 +1,94 @@
-# tpack 介绍
-tpack 是一个使用 NodeJS 开发的项目构建工具。对于前端项目，它可以实现代码预编译、打包、压缩等功能。
+# TPack 介绍
+TPack 是一个使用 NodeJS 开发的项目构建工具。
+对于前端项目，它可以实现代码预编译、打包、压缩等功能。
 
 ## 下载安装
 
 1. 安装 NodeJS 和 npm。具体请参考 [NodeJS 官网](https://nodejs.org)
-2. 使用 npm 下载 tpack：
+2. 使用 npm 下载 TPack:
 
     > npm install -g tpack
+    > tpack -v                  # 验证 tpack 是否正确安装
   
 ## 首次使用
 
-### 1. 在项目根目录新建 `tpack.js` 文件：              
-
-    var tpack = require("tpack");
-    
-    tpack.task("hello-build", function(){
-        
-        // 定义 .txt 文件的处理方式。
-        tpack.src("*.txt")
-            .pipe(function(file, options, builder){  
-                return file.content.replace(/\s/g, "");
-            })
-            .dest("$1.out");
-        
-        // 开始生成。
-        tpack.build();
-    });
-
-### 2. 执行命令调用：
-
-    > cd <项目根目录>
-    > tpack hello-build
-
-### 3. 对示例的解释
-
-示例中，创建了一个名为 `hello-build` 的任务，然后通过命令行调用。
-
-任务的内容为：遍历项目中所有 .txt 文件，删除其内容的空格然后保存为同名的 .out 文件。
-
-## 实时编译(增量发布)
-
-如果需要在文件保存后自动重新生成，修改上述示例为：
+### 1. 在项目根目录新建 `tpack.config.js` 文件：              
 
     var tpack = require("tpack");
 
     // 定义 .txt 文件的处理方式。
     tpack.src("*.txt")
         .pipe(function(file, options, builder){  
-            return file.content.replace(/\s/g, "");
+            file.content = file.content.replace(/\s/g, "");
         })
         .dest("$1.out");
-
-    tpack.task("hello-build", function(){
-        // 开始生成。
-        tpack.build();
+    
+    // 定义生成任务。
+    tpack.task("hello", function(options){
+        tpack.build(options);
     });
 
-    tpack.task("hello-watch", function(){
-        // 开始监听。
-        tpack.watch();
-    });
-    
-然后通过命令行调用即可：
-    
+### 2. 执行命令调用：
+
     > cd <项目根目录>
-    > tpack hello-watch
+    > tpack hello
+
+### 3. 对示例的解释
+
+示例中，创建了一个名为 `hello` 的任务，然后通过命令行调用。
+
+任务的内容为：遍历项目中所有 .txt 文件，删除其内容的空格然后保存为同名的 .out 文件。
+
+## 实时编译(增量发布)
+
+添加如下代码：
+
+    tpack.task("watch", function(options){
+        tpack.watch(options);
+    });
+    
+然后通过命令行调用：
+    
+    > tpack watch
+
+即可监听所有 *.txt 文件并在文件更新后重新生成。
 
 ## Web 服务器
 
-tpack 可以自启服务器，此服务器可以在响应时自动生成。
+TPack 自带 Web 服务器，通过这个服务器，可以在请求时自动生成。
 
-通过服务器可以实现和实时编译相同的功能，并且稳定性高。
-
-    tpack.task("hello-server", function(){
-        // 启动服务器。
+    tpack.task("server", function(){
         tpack.startServer(8080);
     });
 
+然后通过命令行调用：
+    
+    > tpack server
 
-## tpack 和 gulp 的区别
+在浏览器打开 http://localhost:8080/a.txt，会发现文件内容已经被处理过了。
 
-Gulp 以任务为中心，每个任务会处理若干文件得到新文件。任务之间是独立的，无法互相获取依赖。
+## TPack 和 Gulp/Grunt 的区别
 
-### 相同点
+TPack 更注重跟踪文件的变化。比如 a.less 被更新为 a.css 的时候，html 中引用了
+a.less 的地址会被自动更新。通过此策略，TPack 拥有以下优势：
 
-- 都能定义执行任务。
-- 都能针对特定文件生成新文件。
-- 都能实时编译。
+- 只在文件处理完成后才真正保存，减少硬盘读写时间，加快生成效率。
+- 只需定义每个文件处理的方案，复杂的文件依赖关系就让 TPack 来处理吧。
+- 无论是最终发布，还是开发时自动生成，都能共享配置。
 
-### 不同点
+除此以外，TPack 还作了以下细节优化：
 
-Gulp 将文件列表抽象成流，一个流会经过多个任务依次处理得到结果。如:
+- 只需全局安装一遍，不需要本地安装。
+- 除了 Gulp 提供的 Watch 功能，还提供了更稳定、更快的 Web 服务器方案。
+- 集成了插件常用功能，使插件开发起来更为简单易懂。
+- TPack 推崇文件自描述，几乎不需要配置文件。避免项目变大配置难维护的问题。
+- 考虑多数前端项目需求，提供了已集成常用插件的一键安装程序包：TPack-Web
 
-	gulp.src("**.js")
-		.pipe(uglify())
-		.pipe(rename({ suffix: '.min' }));
+## TPack-Web
 
-以上代码将 js 压缩并重命名为 .min.js。任务完成后流即被更新。
+TPack-Web 插件封装了前端项目的常用功能。只需三个命令，即可快速发布您的前端项目。
 
-
-
-
-- tpack 不需要本地安装。
-
-从总体功能上两者类似，但是实现思路是完全相反的。
-
-Gulp 以任务为中心，每个任务
-
-### 定位
-- grunt/gulp 是一个构架工具，它们只负责处理文件，是一个通用的方案。
-- tpack 的定位是则是专注于前端项目的工具集，发布工具仅仅是它的一部分功能。
-
-### 使用方式
-- gruntfile.js/glup.js 主要用于调用现成的插件来实现需求，随着项目的发展，这个文件会越来越大。
-- tpack.js 主要用于描述需求本身，它应该是通用的，项目初期就确定好的，不需要经常更新。
-
-## tpack-web
-
-tpack-web 插件封装了前端项目的常用功能。只需三个命令，即可快速发布您的前端项目。
-
-    > npm install -g tpack-web              # 安装 tpack-web
+    > npm install -g tpack-web              # 安装 TPack-web
     > cd <要发布的项目根目录>
     > tpack-web -out ../build/
 
@@ -132,8 +101,8 @@ tpack-web 插件封装了前端项目的常用功能。只需三个命令，即�
 5. 压缩 Css/Js 文件。
 6. 为 Css/Js 引用路径追加时间戳以避免缓存。
 
-除此之外，tpack-web 还提供了：实时编译、模块自动加载、上传到 CDN、AJAX 接口模拟、占位图等功能。
-具体请参考 [tpack-web 主页](https://github.com/tpack/tpack-web)
+除此之外，TPack-Web 还提供了：实时编译、模块自动加载、上传到 CDN、AJAX 接口模拟、占位图等功能。
+具体请参考 [TPack-Web 主页](https://github.com/tpack/tpack-web)
 
 ## 文档
 
--- test/tpack.config.js
+++ test/tpack.config.js
@@ -14,119 +14,84 @@ tpack.verbose = true;
 tpack.loadIgnoreFile(".gitignore");
 tpack.ignore(".*", "_*", "$*", "*~", "tpack*");
 
-// 所有任务都需要先执行以下预编译的规则。
-tpack.src("*.scss", "*.sass").pipe(require("tpack-sass")).dest("$1.css");
-tpack.src("*.less").pipe(require("tpack-less")).pipe(require("tpack-autoprefixer")).dest("$1.css");
-tpack.src("*.es", "*.es6", "*.jsx").pipe(require("tpack-babel")).dest("$1.js");
-tpack.src("*.coffee").pipe(require("tpack-coffee-script")).dest("$1.js");
+function initRules(build, watch, server){
 
-// libs 和 include 发布时忽略。
-tpack.src("libs/*", "include/*").dest(null);
+    // 所有任务都需要先执行以下预编译的规则。
+    tpack.src("*.scss", "*.sass").pipe(require("tpack-sass")).dest("$1.css");
+    tpack.src("*.less").pipe(require("tpack-less")).pipe(require("tpack-autoprefixer")).dest("$1.css");
+    tpack.src("*.es", "*.es6", "*.jsx").pipe(require("tpack-babel")).dest("$1.js");
+    tpack.src("*.coffee").pipe(require("tpack-coffee-script")).dest("$1.js");
 
-function assets(build) {
+    // libs 和 include 发布时忽略。
+    tpack.src("libs/*", "include/*").dest(null);
 
-    // 解析资源文件中的 require 和 #include。
-    tpack.src("*.html", "*.htm", "*.css", "*.js").pipe(require("tpack-assets"), {
+    if(build || watch) {
+        // 资源文件夹下的文件统一使用 md5 命名。并重命名到 cdn_upload 目录。
+        tpack.src(/^((scripts|styles|images|fonts|resources)\/([^\/]*\/)*[^\.]*?)\.(.*)$/i).dest("cdn_upload/$1_<md5_6>.$4");
+        tpack.addCdnUrl("cdn_upload", "http://cdn.com/assets");
+    }
 
-        // 路径中 / 表示的实际路径。
-        rootPath: "",
-
-        // 是否解析  ?__inline 指令。 'auto' 表示自动判断。
-        inline: 'auto',
-
-        // 路径中 // 表示的协议。
-        protocal: '',
-
-        // require 全局搜索路径。如果未指定则不启用全局搜索功能，所有路径均为相对路径。
-        paths: ["libs"],
-
-        // 是否将目录层级上的所有 node_modules 文件夹追加到全局搜索路径。
-        searchNodeModules: true,
+    // 解析 CSS 和 JS 内的模块化部分。
+    tpack.src("*.js", "*.css").pipe(require("tpack-assets"), {
 
         // require 未包含扩展名时，尝试自动追加的扩展名。
         extensions: ['.json', '.jsx', '.es', '.es6', '.coffee', '.js', '.scss', '.less', '.css'],
-        extensions: ['.js', '.css'],
-
-        // 解析 ?__url 指令。
-        resolveUrls: true,
-
-        urlPostfix: "_=<date>",
-
-
-
-
-        // 是否允许请求远程服务器的模块。
-        requestRemoteModules: true,
-
-
-        // 解析 #include 指令。
-        parseInclude: true,
-
-        // 解析 __dest 导出指令。
-        parseDest: false,
-
-        // 解析 <style> <script> 等标签。
-        parseTags: true,
-
-        // 解析 ?__inline 指令。
-        parseInline: false,
-
-        // 解析 CommonJs require 指令。
-        parseCmdRequire: true,
-
-        // 解析 AMD require 指令。
-        parseAmdRequire: true,
-
-        // 解析 CSS 地址指令。
-        parseCssUrl: true,
+            
+        // require 全局搜索路径。如果未指定则不启用全局搜索功能，所有路径均为相对路径。
+        paths: ["libs"],
 
         // 将包含以下扩展名的文件自动导出到外部文件，而非放入当前文件。
         exports: {
-            '.css': '../styles/$0.css',
-            '.css': '../styles/$0.css',
-        },
+            // 导出 JS 中依赖的 CSS 文件到同名 CSS 文件。
+            'css': '../styles/$1.css',
+            // 导出 JS 中依赖的 CSS文件到同名 
+            'resources': '../images/',
+        }
 
         // 自动排除以下文件。
         exclude: ["assets/common.js"]
     });
 
+    if(build) {
+        // 压缩 CSS 和 JS。
+        tpack.src("*.css").pipe(require('tpack-clean-css'));
+        tpack.src("*.js").pipe(require('tpack-uglify-js'));
+    }
+
+    // 处理 HTML 内依赖。
+    tpack.src("*.html", "*.htm").pipe(require("tpack-assets"), {
+        inline: server ? false : true
+    });
+
+    if(build) {
+
+        // 直接生成文件。
+        tpack.src().pipe(function (file, options, builder) {
+            return "此项目是从 " + builder.srcPath + " 生成的，不要修改！生成时间：" + new Date()
+        }).dest("NOTE.txt");
+
+        // 合并特定文件。
+        tpack.src("full-test.html").pipe(require('tpack-concat')).dest("full-test-2.html");
+
+    }
+
 }
 
 // 生成任务。
 tpack.task('build', function (options) {
-
-    assets(true);
-
-    // 压缩 CSS 和 JS。
-    tpack.src("*.css").pipe(require('tpack-clean-css'));
-    tpack.src("*.js").pipe(require('tpack-uglify-js'));
-
-    // 资源文件夹下的文件统一使用 md5 命名。并重命名到 cdn_upload 目录。
-    tpack.src(/^((scripts|styles|images|fonts|resources)\/([^\/]*\/)*[^\.]*?)\.(.*)$/i).dest("cdn_upload/$1_<md5_6>.$4");
-    tpack.addCdn("cdn_upload", "http://cdn.com/assets");
-
-    // 合并特定 JS 文件。
-    tpack.src("full-test.html", "full-test.html").pipe(require('tpack-concat')).dest("full-test-2.html");
-
-    // 直接生成文件。
-    tpack.src().pipe(function (file, options, builder) {
-        return "此项目是从 " + builder.srcPath + " 生成的，不要修改！生成时间：" + new Date()
-    }).dest("NOTE.txt");
-
-    // 开始根据之前定制的所有规则开始生成操作。
+    initRules(true, false, false);
     tpack.build(options);
-
 });
 
 // 监听任务。
 tpack.task('watch', function (options) {
-    assets(true);
+    initRules(true, false, false);
     tpack.watch(options.path);
 });
 
 // 服务器任务。
 tpack.task('server', function (options) {
-    assets(false);
+    initRules(false, false, true);
     tpack.startServer(options);
 });
 
